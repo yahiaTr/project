dc03e9cca53f595f512640688fcd79e021a9e073
yahiaTr <tryahia20@gmail.com>
2025-02-17 12:15:21 -0800

    Updated MongoDB connection string

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const ConnectDB = async() => {
     try{
-        await mongoose.connect("mongodb://localhost:/aiProject");
+        await mongoose.connect("mongodb+srv://tryahiamo99:<6kWEnVzs4tv7cezP>@cluster0.yb7xz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         console.log("connected to mongoDB");
 
     }catch(error){
