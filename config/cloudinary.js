e6556a20f2034e98d813da8a147681c581946a58
yahiaTr <tryahia20@gmail.com>
2025-02-18 12:47:32 +0300

    Updated MongoDB connection string

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: dcp3lgmxj,
+  cloud_name: "dcp3lgmxj",
   api_key: 939718721375167,
-  api_secret: r0pirXxVOBg4sgTap2uutI_Un34,
+  api_secret: "r0pirXxVOBg4sgTap2uutI_Un34",
 });
 
 module.exports = cloudinary;
